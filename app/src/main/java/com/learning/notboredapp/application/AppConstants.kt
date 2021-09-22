1d8b8a12913eb2de49689ae9e7c87c137bbbbe39
BrayanGarcia0 <brayan.garcia@mercadolibre.com.co>
2021-09-22 14:35:27 -0500

    Rest Service Model finished

---
+++ app/src/main/java/com/learning/notboredapp/application/AppConstants.kt
@@ -0,0 +1,5 @@
+package com.learning.notboredapp.application
+
+object AppConstants {
+    const val BASE_URL: String = "http://www.boredapi.com/api/activity"
+}
--- app/src/main/java/com/learning/notboredapp/repository/ActivityRepositoryImpl.kt
+++ app/src/main/java/com/learning/notboredapp/repository/ActivityRepositoryImpl.kt
@@ -1,14 +1,11 @@
 package com.learning.notboredapp.repository
 
 import com.learning.notboredapp.data.model.SuggestedActivity
+import com.learning.notboredapp.data.remote.SuggestedActivityDataSource
 
-class ActivityRepositoryImpl: IActivityRepository {
-    override suspend fun getActivityByType(type: String): SuggestedActivity {
-        TODO("Not yet implemented")
-    }
+class ActivityRepositoryImpl(private val dataSource: SuggestedActivityDataSource): IActivityRepository {
+    override suspend fun getActivityByType(type: String): SuggestedActivity = dataSource.getActivityByType(type)
 
-    override suspend fun getRandomActivity(): SuggestedActivity {
-        TODO("Not yet implemented")
-    }
+    override suspend fun getRandomActivity(participants: Int): SuggestedActivity = dataSource.getRandomActivity(participants)
 
 }
